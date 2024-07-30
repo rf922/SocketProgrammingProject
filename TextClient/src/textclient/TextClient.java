10599681a37ae5ec948f1c5de6cb45cfb9fdbcd1
rf922 <rfabiani@sfsu.edu>
2024-07-30 10:51:10 -0700

    connect TextClient to TextServer, start working on prompt during connection

--- TextClient/src/textclient/TextClient.java
+++ TextClient/src/textclient/TextClient.java
@@ -6,13 +6,35 @@
 package textclient;
 
 
-public class TextClient {
+import java.io.*;
+import java.net.*;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class TextClient {
+    private static final int PORT_NUM = 1212;
+    private static final String HOST = "localhost";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String message;
+        Scanner sc = new Scanner(System.in);
+        try(Socket textClientSocket = new Socket(HOST, PORT_NUM);){            
+            System.out.println("Connected to Server ");
+            Scanner scn = new Scanner(textClientSocket.getInputStream());
+            PrintWriter pw = new PrintWriter(textClientSocket.getOutputStream(), true);
+            while(true){
+                System.out.println("Enter a message: ");
+                message = sc.nextLine();
+                pw.println(message);
+                System.out.println(scn.nextLine());
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
     
 }
--- TextServer/src/textserver/TextServer.java
+++ TextServer/src/textserver/TextServer.java
@@ -13,17 +13,24 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class TextServer {
-
+    
+    private static final int PORT_NUM = 1212;
+    private static final String PROMPT = String.format("", "");
+    
     private static final HashMap<String, String> users = new HashMap<>();
     private static final HashMap<String, ArrayList<Message>> userMessages = new HashMap<>();
-    
+
+    /**
+     * Message class to handle user to user messages
+     */
     private static class Message implements Comparable<Message> {
+
         private static final String border = "===========================";
         private LocalDateTime date;
         private String sender;
         private String message;
-        
-        public Message(LocalDateTime date, String sender, String message){
+
+        public Message(LocalDateTime date, String sender, String message) {
             this.date = date;
             this.sender = sender;
             this.message = message;
@@ -40,17 +47,26 @@ public class TextServer {
                 sender, message);
             return msg;
         }
-        
- 
-        
-        
+
     }
-    
+
+    /**
+     * initializer block to populate user hash map & userMessages Hashmap
+     */
     static {
         users.put("Rf922", "secret-key01");
         users.put("Yetem", "secret-key02");
         users.put("Keev", "secret-key03");
         users.put("Don", "secret-key04");
+        users.put("sean", "secret-key05");
+
+        userMessages.put("Rf922", new ArrayList<>());
+        userMessages.get("Rf922").add(new Message(LocalDateTime.now(), "Sean", "Down for Math book club ? 4:00 P.M Wednday @ the pub "));
+        userMessages.get("Rf922").add(new Message(LocalDateTime.now(), "Yetem", "wyd"));
+
+        userMessages.put("Yetem", new ArrayList<>());
+        userMessages.get("Yetem").add(new Message(LocalDateTime.now(), "Sean", "Down for Math book club ? 4:00 P.M Wednday @ the pub "));
+
     }
 
     /**
@@ -58,28 +74,29 @@ public class TextServer {
      */
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newCachedThreadPool();
-        try(ServerSocket textServerSocket = new ServerSocket(1212)){
+        try (ServerSocket textServerSocket = new ServerSocket(PORT_NUM)) {
             System.out.println("Server is running...");
-            while(true){
+            while (true) {
                 Socket clientSocket = textServerSocket.accept();
-                executorService.submit(() -> handleClient(clientSocket));                
+                executorService.submit(() -> handleClient(clientSocket));
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-        executorService.shutdown();}
+        } finally {
+            executorService.shutdown();
+        }
     }
-    
-    public static void handleClient(Socket socket){
-        try(PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
-            ){
-            
+
+    public static void handleClient(Socket socket) {
+        try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
             String prompt = String.format("");
-            out.println("");
-        }catch(IOException e) {
+            out.println("Prompt here");
+            String res = in.readLine();
             
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
