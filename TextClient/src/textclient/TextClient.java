8b4aa70648ad8c301989df99d57cab43b3e82bf7
rf922 <rfabiani@sfsu.edu>
2024-07-30 02:48:37 -0700

    initial commit for socket programming project, initialize both socket and server java projects

---
+++ TextClient/src/textclient/TextClient.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ * 
+ * 
+ */
+package textclient;
+
+
+public class TextClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ TextServer/src/textserver/TextServer.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ * 
+ * 
+ */
+package textserver;
+
+
+public class TextServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
